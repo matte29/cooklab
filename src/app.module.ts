2709c32f2efd8c71352542543b7a3172fcb24268
Matthew Ellis <matthewaellis29@gmail.com>
2020-06-29 10:43:11 -0400

    Added recipes module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+    imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, RecipesModule],
 })
 export class AppModule {}
