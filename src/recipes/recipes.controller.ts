5c2ac2ae485032d0b834f00136d03036cb26cecc
Matthew Ellis <matthewaellis29@gmail.com>
2020-06-30 14:39:57 -0400

    Added path guard and getUser guard to recipe controller

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -1,14 +1,18 @@
-import { Logger, Body } from '@nestjs/common';
+import { Logger, Body, Post, Controller, UseGuards } from '@nestjs/common';
 import { Recipe } from './recipe.entity';
 import { CreateRecipeDto } from './dto/create-recipe-dto';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from '../auth/user.entity';
 import { RecipesService } from './recipes.service';
+import { AuthGuard } from '@nestjs/passport';
 
+@Controller('recipes')
+@UseGuards(AuthGuard())
 export class RecipesController {
     private logger = new Logger('RecipesController');
     constructor(private recipesService: RecipesService) {}
 
+    @Post('/')
     createRecipe(
         @Body() createRecipeDto: CreateRecipeDto,
         @GetUser() user: User,
@@ -16,7 +20,7 @@ export class RecipesController {
         this.logger.verbose(
             `User "${
                 user.username
-            }" creating a new task. Data: ${JSON.stringify(createRecipeDto)}`,
+            }" creating a new recipe. Data: ${JSON.stringify(createRecipeDto)}`,
         );
 
         return this.recipesService.createRecipe(createRecipeDto, user);
