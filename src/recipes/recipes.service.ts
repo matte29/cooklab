9308ecd566c969228d55c1d6854673310beb6386
Matthew Ellis <matthewaellis29@gmail.com>
2020-07-01 11:51:42 -0400

    Added removeRecipe to recipe service

--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -3,7 +3,7 @@ import { RecipeRepository } from './recipes.repository';
 import { CreateRecipeDto } from './dto/create-recipe-dto';
 import { User } from '../auth/user.entity';
 import { Recipe } from './recipe.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { GetRecipeFilterDto } from './dto/get-recipe-filter-dto';
 
 @Injectable()
@@ -24,5 +24,16 @@ export class RecipesService {
         user: User,
     ): Promise<Recipe> {
         return this.recipeRepositoy.createRecipe(createRecipeDto, user);
+    }
+
+    async removeRecipe(rid: number, user: User): Promise<void> {
+        const result = await this.recipeRepositoy.delete({
+            rid,
+            userId: user.id,
+        });
+
+        if (result.affected === 0) {
+            throw new NotFoundException(`Recipe with Id "${rid}" not found`);
+        }
     }
 }
