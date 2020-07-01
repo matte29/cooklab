875074785f283eb13720e8b11a6f461f1f9ccd20
Matthew Ellis <matthewaellis29@gmail.com>
2020-07-01 11:16:21 -0400

    Added getRecipes to recipes repo

--- src/recipes/recipes.repository.ts
+++ src/recipes/recipes.repository.ts
@@ -3,11 +3,55 @@ import { Recipe } from './recipe.entity';
 import { User } from '../auth/user.entity';
 import { CreateRecipeDto } from './dto/create-recipe-dto';
 import { Logger, InternalServerErrorException } from '@nestjs/common';
+import { GetRecipeFilterDto } from './dto/get-recipe-filter-dto';
 
 @EntityRepository(Recipe)
 export class RecipeRepository extends Repository<Recipe> {
     private logger = new Logger('RecipeRepository');
 
+    async getRecipes(
+        filterDto: GetRecipeFilterDto,
+        user: User,
+    ): Promise<Recipe[]> {
+        const { title, type, hours, author } = filterDto;
+        const query = this.createQueryBuilder('recipe');
+
+        if (title) {
+            query.andWhere('(recipe.title LIKE :title)', {
+                title: `%${title}%`,
+            });
+        }
+        if (type) {
+            query.andWhere('(recipe.dishType LIKE :type)', {
+                type: `%${type}%`,
+            });
+        }
+        if (hours) {
+            query.andWhere('(recipe.hours LIKE :hours)', {
+                hours: `%${hours}%`,
+            });
+        }
+        // if (author) {
+        //     query.andWhere('(recipe.user LIKE :author)', {
+        //         author: `%${author}%`,
+        //     });
+        // }
+
+        try {
+            const recipes = await query.getMany();
+            return recipes;
+        } catch (error) {
+            this.logger.error(
+                `User "${
+                    user.username
+                }", failed to get recipes. Filters: ${JSON.stringify(
+                    filterDto,
+                )}`,
+                error.stack,
+            );
+            throw new InternalServerErrorException();
+        }
+    }
     async createRecipe(
         createRecipeDto: CreateRecipeDto,
         user: User,
