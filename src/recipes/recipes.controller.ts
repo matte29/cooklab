ca02bc762399ab9ae3ee418998c9856d3872fd02
Matthew Ellis <matthewaellis29@gmail.com>
2020-07-01 11:15:54 -0400

    Added getRecipes to recipes controller

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -1,16 +1,39 @@
-import { Logger, Body, Post, Controller, UseGuards } from '@nestjs/common';
+import {
+    Logger,
+    Body,
+    Post,
+    Controller,
+    UseGuards,
+    Get,
+    Query,
+    ValidationPipe,
+} from '@nestjs/common';
 import { Recipe } from './recipe.entity';
 import { CreateRecipeDto } from './dto/create-recipe-dto';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from '../auth/user.entity';
 import { RecipesService } from './recipes.service';
 import { AuthGuard } from '@nestjs/passport';
+import { GetRecipeFilterDto } from './dto/get-recipe-filter-dto';
 
 @Controller('recipes')
 @UseGuards(AuthGuard())
 export class RecipesController {
     private logger = new Logger('RecipesController');
     constructor(private recipesService: RecipesService) {}
+
+    @Get('/')
+    getTasks(
+        @Query(ValidationPipe) filterDto: GetRecipeFilterDto,
+        user: User,
+    ): Promise<Recipe[]> {
+        this.logger.verbose(
+            `User "${
+                user.username
+            }" retrieving all tasks. Filters: ${JSON.stringify(filterDto)}`,
+        );
+        return this.recipesService.getRecipes(filterDto, user);
+    }
 
     @Post('/')
     createRecipe(
