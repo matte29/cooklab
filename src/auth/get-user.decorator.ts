46ae72cd8a98de84a6e428475a67264ef1db2ad0
Matthew Ellis <matthewaellis29@gmail.com>
2020-09-15 19:17:57 -0400

    Fixed import error in get-user decorator

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -2,8 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from './user.entity';
 
 export const GetUser = createParamDecorator(
-    (data, ctx: ExecutionContext): User => {
-        const req = ctx.switchToHttp().getRequest();
+    (data, req): User => {
         return req.user;
     },
 );
